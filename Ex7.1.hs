0c6e7cd69b6175d07b086170ff24206053c63650
hkdnet <satoko.itse@gmail.com>
2018-08-26 10:58:24 +0900

    term2-4 がまともっぽくなった

--- Ex7.1.hs
+++ Ex7.1.hs
@@ -1,23 +1,26 @@
+import Data.List
+
 data Info = I Int Int deriving Show
 type Depth = Int
 type VarName = String
-type Context = Int
+type Context = [String]
 data Term = TmVar Info Int Depth | TmAbs Info VarName Term | TmApp Info Term Term deriving Show
 
 showTerm :: Context -> Term -> String
-showTerm ctx (TmAbs info x t1) = let (ctx', x') = pickFreshName ctx x in "(lambda " ++ x' ++ ". " ++ showTerm ctx t1 ++ ")"
+showTerm ctx (TmAbs info x t1) = let (ctx', x') = pickFreshName ctx x in "(lambda " ++ x' ++ ". " ++ showTerm ctx' t1 ++ ")"
 showTerm ctx (TmApp info t1 t2) = "(" ++ showTerm ctx t1 ++ " " ++ showTerm ctx t2 ++ ")"
-showTerm ctx (TmVar info x depth) = if contextLength ctx == depth then indexToName info ctx x else "[bad index]"
+showTerm ctx (TmVar info x depth) = if contextLength ctx == depth then indexToName ctx x else "[bad index]" ++ show depth ++ "of" ++ show (contextLength ctx)
 
--- 名前を解釈するらしいけどとりあえず数値で出しとく
 pickFreshName :: Context -> String -> (Context, String)
-pickFreshName c t = (c, t)
+pickFreshName c s = case find (\x -> x == s) c of
+  Just e -> pickFreshName c ('\'':s)
+  Nothing -> ((s:c), s)
 
 contextLength :: Context -> Int
-contextLength = id
+contextLength = length
 
-indexToName :: Info -> Context -> Int -> String
-indexToName _ _ s = show s
+indexToName :: Context -> Int -> String
+indexToName ctx n = ctx !! n
 
 termShift :: Depth -> Term -> Term
 termShift d t = walk 0 d t
@@ -29,12 +32,21 @@ walk c d (TmApp fi t1 t2) = TmApp fi (walk c d t1) (walk c d t2)
 
 main :: IO()
 main = do
-  let ctx1 = 1
+  let ctx1 = ["b", "a"]
   let info = I 1 1
   print $ showTerm ctx1 (TmVar info 1 1)
 
+  let ctx2 = ["b","a","z","y","x"]
+  -- \w.\a.x
+  let term2 = TmApp info (TmVar info 4 5) (TmApp info (TmVar info 3 5) (TmVar info 2 5))
+  print $ showTerm ctx2 term2
+  let term3 = TmAbs info "w" (TmApp info (TmVar info 4 6) (TmVar info 0 6))
+  print $ showTerm ctx2 term3
+  let term4 = TmAbs info "w" (TmAbs info "a" (TmVar info 4 7))
+  print $ showTerm ctx2 term4
+
   -- ↑2 (\. \. 1 (0 2))
-  let ctx2 = 2
-  let term = TmAbs info "1" (TmAbs info "0" ( TmApp info (TmVar info 1 2) (TmApp info (TmVar info 0 2) (TmVar info 2 2))))
-  print $ showTerm ctx2 term -- "(lambda 1. (lambda 0. (1 (0 2))))"
+  let ctx3 = ["a"]
+  let term3 = TmAbs info "a" (TmAbs info "a" ( TmApp info (TmVar info 1 3) (TmApp info (TmVar info 0 3) (TmVar info 2 3))))
+  print $ showTerm ctx3 term3
 
